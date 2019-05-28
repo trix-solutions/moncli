8c5d54ee65bca0f0b8311eca76ac3eba8c5dbcb3
patrick_tucker_fortimize <patrick.tucker@fortimize.com>
2019-05-28 12:28:47 -0400

    Commit constants for route signatures.

--- moncli/routes.py
+++ moncli/routes.py
@@ -4,7 +4,54 @@ BASE_URL = 'https://api.monday.com'
 PORT = '443'
 API_VERSION = 'v1'
 
-def get_users(api_key, page = 0, per_page = 25, offset = 0, order_by_latest = false):
+# User route signatures
+USERS = 'users.json'
+USER_BY_ID = 'users/{}.json'
+USER_POSTS = 'users/{}/posts.json'
+USER_NEWSFEED = 'users/{}/newsfeed.json'
+USER_UNREAD_FEED = 'users/{}/unread_feed'
+
+# Updates route signatures
+UPDATES = 'updates.json'
+UPDATES_BY_ID = 'updates/{}.json'
+UPDATE_LIKE = 'updates/{}/like.json'
+UPDATE_UNLIKE = 'updates/{}/unlike.json'
+
+# Pulses route signatures
+PULSES = 'pulses.json'
+PULSE_BY_ID = 'pulses/{}.json'
+PULSE_SUBSCRIBERS = 'pulses/{}/subscribers.json'
+PULSE_SUBSCRIBERS_BY_USER_ID = 'pulses/{}/subscribers/{}.json'
+PULSE_NOTES = 'pulses/{}/notes.json'
+PULSE_NOTE_BY_ID = 'pulses/{}/notes/{}.json'
+PULSE_UPDATES = 'pulses/{}/updates.json'
+
+# Boards route signatures
+BOARDS = 'boards.json'
+BOARD_BY_ID = 'boards/{}.json'
+BOARD_GROUPS = 'boards/{}/groups.json'
+BOARD_MOVE_GROUP = 'boards/{}/groups/{}/move.json'
+BOARD_GROUP_BY_ID = 'boards/{}/groups/{}.json'
+BOARD_COLUMNS = 'boards/{}/columns.json'
+BOARD_COLUMN_BY_ID = 'boards/{}/columns/{}.json'
+BOARD_VALUE_COLUMN = 'boards/{}/columns/{}/value.json'
+BOARD_TEXT_COLUMN = 'boards/{}/columns/{}/text.json'
+BOARD_PERSON_COLUMN = 'boards/{}/columns/{}/person.json'
+BOARD_STATUS_COLUMN = 'boards/{}/columns/{}/status.json'
+BOARD_DATE_COLUMN = 'boards/{}/columns/{}/date.json'
+BOARD_NUMERIC_COLUMN = 'boards/{}/columns/{}/numeric.json'
+BOARD_TAGS_COLUMN = 'boards/{}/columns/{}/tags.json'
+BOARD_TIMELINE_COLUMN = 'boards/{}/columns/{}/timeline.json'
+BOARD_PULSES = 'boards/{}/pulses.json'
+BOARD_DUPLICATE_PULSE = 'boards/{}/pulses/{}/duplicate.json'
+BOARD_MOVE_PULSES = 'boards/{}/pulses/move.json'
+BOARD_SUBSCRIBERS = 'boards/{}/subscribers.json'
+BOARD_SUBSCRIBERS_BY_ID = 'boards/{}/subscribers/{}.json'
+
+# Tags route signatures
+TAGS_BY_ID = 'tags/{}.json'
+
+def get_users(api_key, page = 0, per_page = 25, offset = 0, order_by_latest = False):
 
     resource_url = '{}/users.json'.format(format_url())
 
@@ -22,6 +69,20 @@ def get_users(api_key, page = 0, per_page = 25, offset = 0, order_by_latest = fa
         return resp.json()
 
     raise_mondayapi_error('GET', 'users.json', resp)
+
+
+def get_user_by_id(api_key, user_id):
+
+    resource_url = '{}/users/{}.json'.format(
+        format_url(),
+        user_id)
+
+    params = {
+        'api_key': api_key,
+        'id': user_id
+    }
+
+
 
 def get_boards(api_key, per_page = 25, only_globals = False, order_by_latest = False):
 
