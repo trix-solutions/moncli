6dcfc77b5f139fec89cfa1e021abc8e08f6b2aa9
ashatz <ashatz.hero@gmail.com>
2019-06-01 02:55:18 -0400

    Initial commit for board constructor.

--- moncli/boards.py
+++ moncli/boards.py
@@ -1,6 +1,19 @@
+from datetime import datetime
+
 from moncli.routes import boards 
+from .constants import DATE_FORMAT
 
 class Board():
 
-    pass
+    def __init__(self, resp):
+
+        self.id = resp['id']
+        self.name = resp['name']
+        self.description = resp['description']
+        self.board_kind = resp['board_kind']
+        self.created_at = datetime.strptime(resp['created_at'], DATE_FORMAT)
+        self.updated_at = datetime.strptime(resp['updated_at'], DATE_FORMAT)
+        self.columns = resp['columns']
+        self.groups = resp['groups']
+
     
