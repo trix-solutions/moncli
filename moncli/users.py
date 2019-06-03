58a4a682b118e835429a86076a05c8bd4c3a47b2
ashatz <ashatz.hero@gmail.com>
2019-06-03 01:17:31 -0400

    Add user class to the mix.

---
+++ moncli/users.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from .constants import DATE_FORMAT
+
+class User():
+
+    def __init__(self, resp):
+
+        self.id = resp['id']
+        self.name = resp['name']
+        self.username = resp['email']
+        self.title = resp['title']
+        self.position = resp['position']
+        self.phone = resp['phone']
+        self.location = resp['location']
+        self.status = resp['status']
+        self.birthday = resp['birthday']
+        self.is_guest = resp['is_guest']
+        self.created_at = datetime.strptime(resp['created_at'], DATE_FORMAT)
+        self.updated_at = datetime.strptime(resp['updated_at'], DATE_FORMAT)
