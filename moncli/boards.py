4f87b42178492e28a96011b11b380c1eb9be1e7c
ashatz <ashatz.hero@gmail.com>
2019-06-05 00:18:19 -0400

    Define group and column objects for boards

--- moncli/boards.py
+++ moncli/boards.py
@@ -1,23 +1,46 @@
 from datetime import datetime
 
 from moncli.routes import boards 
-from .constants import DATE_FORMAT
+from .constants import DATE_FORMAT, COLUMN_COLOR
 
 class Board():
 
-    def __init__(self, resp):
+    def __init__(self, data):
 
-        self.__data = resp
+        self.__data = data
 
-        self.id = resp['id']
-        self.name = resp['name']
-        self.description = resp['description']
-        self.board_kind = resp['board_kind']
-        self.created_at = datetime.strptime(resp['created_at'], DATE_FORMAT)
-        self.updated_at = datetime.strptime(resp['updated_at'], DATE_FORMAT)
-        self.columns = resp['columns']
-        self.groups = resp['groups']
+        self.id = data['id']
+        self.name = data['name']
+        self.description = data['description']
+        self.board_kind = data['board_kind']
+        self.created_at = datetime.strptime(data['created_at'], DATE_FORMAT)
+        self.updated_at = datetime.strptime(data['updated_at'], DATE_FORMAT)
+        self.columns = [Column(column_data) for column_data in data['columns']]
+        self.groups = [Group(group_data) for group_data in data['groups']]
 
+class Column():
+
+    def __init__(self, data):
+
+        self.__data = data
+
+        self.id = data['id']
+        self.title = data['title']
+        self.type = data['type']
+        
+        if self.type == COLUMN_COLOR:
+            self.labels = data['labels']
+
+
+class Group():
+
+    def __init__(self, data):
+
+        self.__data = data
+
+        self.id = data['id']
+        self.title = data['title']
+        self.board_id = data['board_id']
 
 
     
