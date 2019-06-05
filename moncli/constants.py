27ad54f7d78cf9c7e491ec3f4e9b6a8c0b81c762
ashatz <ashatz.hero@gmail.com>
2019-06-05 00:18:01 -0400

    Add initial round of column data types

--- moncli/constants.py
+++ moncli/constants.py
@@ -1,1 +1,10 @@
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+# Column Types
+COLUMN_NAME = 'name'
+COLUMN_MULTIPERSON = 'multi-person'
+COLUMN_COLOR = 'color'
+COLUMN_DATE = 'date'
+COLUMN_NUMERIC = 'numeric'
+COLUMN_DURATION = 'duration'
+COLUMN_LONGTEXT = 'long-text'
