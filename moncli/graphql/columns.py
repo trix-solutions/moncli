2bd4dedef0a50677932f9294c65b259925a412c3
AShatz <andrew.shatz@trix.solutions>
2019-08-03 16:53:08 -0400

    Commit get columns from board

---
+++ moncli/graphql/columns.py
@@ -0,0 +1,15 @@
+from moncli.graphql import constants, GraphQLOperation, OperationType
+from moncli.graphql.requests import execute_query
+
+
+def get_board_columns(api_key: str, board_id: str):
+
+    operation = GraphQLOperation(OperationType.QUERY, constants.BOARDS, ids=int(board_id))
+    columns = operation.add_fields('columns').get_field(constants.BOARDS + '.columns')
+    columns.add_fields('id', 'title', 'type', 'archived', 'settings_str', 'width')
+
+    result = execute_query(api_key, operation=operation)
+    return result['boards'][0]['columns']
+
+    
+
