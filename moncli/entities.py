58826fdc7b757abc874c325901bcd5b424250d51
AShatz <andrew.shatz@trix.solutions>
2019-09-26 13:46:32 -0400

    Add creds to Group

--- moncli/entities.py
+++ moncli/entities.py
@@ -558,8 +558,7 @@ class Column():
 class Group():
 
     def __init__(self, **kwargs):
-        self.__api_key_v1 = kwargs['api_key_v1']
-        self.__api_key_v2 = kwargs['api_key_v2']
+        self.__creds = kwargs['creds']
         self.id = kwargs['id']
         self.board_id = kwargs['board_id']
         self.__item_ids = None
@@ -588,14 +587,13 @@ class Group():
     def duplicate(self, group_title: str, add_to_top: bool = False, *argv):
         
         group_data = client.duplicate_group(
-            self.__api_key_v2, 
+            self.__creds.api_key_v2, 
             self.board_id, 
             self.id, 
             *argv)
 
         return Group(
-            api_key_v1=self.__api_key_v1,
-            api_key_v2=self.__api_key_v2,
+            creds=self.__creds,
             board_id=self.board_id,
             **group_data)
 
@@ -603,14 +601,13 @@ class Group():
     def archive(self, *argv):
         
         group_data = client.archive_group(
-            self.__api_key_v2,
+            self.__creds.api_key_v2,
             self.board_id,
             self.id,
             *argv)
 
         return Group(
-            api_key_v1=self.__api_key_v1,
-            api_key_v2=self.__api_key_v2,
+            creds=self.__creds,
             board_id=self.board_id,
             **group_data)
 
@@ -618,14 +615,13 @@ class Group():
     def delete(self, *argv):
         
         group_data = client.delete_group(
-            self.__api_key_v2,
+            self.__creds.api_key_v2,
             self.board_id,
             self.id,
             *argv)
 
         return Group(
-            api_key_v1=self.__api_key_v1,
-            api_key_v2=self.__api_key_v2,
+            creds=self.__creds,
             board_id=self.board_id
             **group_data)
 
@@ -633,22 +629,19 @@ class Group():
     def add_item(self, item_name: str, column_values: str = None, *argv):
         
         item_data = client.create_item(
-            self.__api_key_v2,
+            self.__creds.api_key_v2,
             item_name,
             self.board_id,
             *argv,
             column_values=column_values)
 
-        return Item(
-            api_key_v1=self.__api_key_v1,
-            api_key_v2=self.__api_key_v2,
-            **item_data)
+        return Item(creds=self.__creds, **item_data)
 
 
     def get_items(self):
 
         items_resp = client.get_items(
-            self.__api_key_v2, 
+            self.__creds.api_key_v2, 
             'id',
             'name',
             'board.id',
@@ -665,13 +658,13 @@ class Group():
             ids=self.__item_ids, 
             limit=1000)
 
-        return [Item(self.__api_key_v1, self.__api_key_v2, **item_data) for item_data in items_resp] 
+        return [Item(creds=self.__creds, **item_data) for item_data in items_resp] 
 
 
     def get_items_by_column_values(self, column_id: str, column_value: str, *argv, **kwargs):
         
         items_data = client.get_items_by_column_values(
-            self.__api_key_v2,
+            self.__creds.api_key_v2,
             self.board_id,
             self.id,
             column_value,
@@ -679,7 +672,7 @@ class Group():
             **kwargs)
 
         return [
-            Item(api_key_v1=self.__api_key_v1, api_key_v2=self.__api_key_v2, **item_data)
+            Item(creds=self.__creds, **item_data)
             for item_data
             in items_data
         ]
@@ -688,41 +681,32 @@ class Group():
     def move_item(self, item_id: str, group_id: str, *argv):
         
         item_data = client.move_item_to_group(
-            self.__api_key_v2, 
+            self.__creds, 
             item_id, 
             group_id,
             *argv)
 
-        return Item(
-            api_key_v1=self.__api_key_v1,
-            api_key_v2=self.__api_key_v2,
-            **item_data)
+        return Item(creds=self.__creds, **item_data)
 
 
     def archive_item(self, item_id: str, *argv):
         
         item_data = client.archive_item(
-            self.__api_key_v2,
+            self.__creds.api_key_v2,
             item_id,
             *argv)
 
-        return Item(
-            api_key_v1=self.__api_key_v1,
-            api_key_v2=self.__api_key_v2,
-            **item_data)
+        return Item(creds=self.__creds, **item_data)
 
     
     def delete_item(self, item_id: str, *argv):
         
         item_data = client.delete_item(
-            self.__api_key_v2,
+            self.__creds.api_key_v2,
             item_id, 
             *argv)
             
-        return Item(
-            api_key_v1=self.__api_key_v1,
-            api_key_v2=self.__api_key_v2,
-            **item_data)
+        return Item(creds=self.__creds, **item_data)
 
 
 class Item():
