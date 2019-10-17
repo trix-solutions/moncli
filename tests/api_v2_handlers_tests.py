bcadd2fd039bf3b8f7202cd0db1c6ed04e971b5f
AShatz <andrew.shatz@trix.solutions>
2019-10-17 12:54:17 -0400

    Implemented first successful unit test

--- tests/api_v2_handlers_tests.py
+++ tests/api_v2_handlers_tests.py
@@ -1,5 +1,7 @@
-from nose.tools import *
-import moncli
+from unittest.mock import patch
+from nose.tools import ok_
+
+from moncli.api_v2 import handlers, constants
 
 def setup():
     print('SETUP')
@@ -8,6 +10,14 @@ def setup():
 def teardown():
     print('TEARDOWN')
 
+@patch('moncli.api_v2.requests.execute_query')
+def test_get_board(execute_query):
+    print('Test Get Board')
+    print('Should return a list of boards')
 
-def test_basic():
-    print('I RAN!', end='')
+    execute_query.return_value = {'boards': [{'id': '1'}, {'id': '2'}, {'id': '3'}, {'id': '4'}, {'id': '5'}]}
+    boards = handlers.get_boards('', 'id', limit=5)
+    
+    ok_(len(boards) == 5)
+
+    print('Test Successful!')
