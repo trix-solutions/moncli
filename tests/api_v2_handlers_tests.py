18b810cabac9fcbab5f14a92b7e0fd96a8403bd1
AShatz <andrew.shatz@trix.solutions>
2019-10-17 13:07:17 -0400

    Add create board test

--- tests/api_v2_handlers_tests.py
+++ tests/api_v2_handlers_tests.py
@@ -2,6 +2,9 @@ from unittest.mock import patch
 from nose.tools import ok_
 
 from moncli.api_v2 import handlers, constants
+from moncli.enums import BoardKind
+
+EXECUTE_QUERY_PATCH = 'moncli.api_v2.requests.execute_query'
 
 def setup():
     print('SETUP')
@@ -10,14 +13,29 @@ def setup():
 def teardown():
     print('TEARDOWN')
 
-@patch('moncli.api_v2.requests.execute_query')
-def test_get_board(execute_query):
-    print('Test Get Board')
-    print('Should return a list of boards')
+@patch(EXECUTE_QUERY_PATCH)
+def test_create_board(execute_query):
 
-    execute_query.return_value = {'boards': [{'id': '1'}, {'id': '2'}, {'id': '3'}, {'id': '4'}, {'id': '5'}]}
+    # Mock setup
+    execute_query.return_value = {constants.CREATE_BOARD: {'id': '1', 'name': 'Test', 'board_kind': 'public'}}
+
+    # Act
+    board = handlers.create_board('', 'Test', BoardKind.public, 'id', 'name', 'board_kind')
+    
+    # Assert
+    ok_(board != None)
+    ok_(board['name'] == 'Test')
+    ok_(board['board_kind'] == 'public')
+
+
+@patch(EXECUTE_QUERY_PATCH)
+def test_get_board(execute_query):
+
+    # Mock setup
+    execute_query.return_value = {constants.BOARDS: [{'id': '1'}, {'id': '2'}, {'id': '3'}, {'id': '4'}, {'id': '5'}]}
+
+    # Act
     boards = handlers.get_boards('', 'id', limit=5)
     
+    # Assert
     ok_(len(boards) == 5)
-
-    print('Test Successful!')
