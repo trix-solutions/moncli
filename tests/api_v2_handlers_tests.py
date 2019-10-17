c22e3391b52ab4edaf537d352bf4e78cd38e1b8c
AShatz <andrew.shatz@trix.solutions>
2019-10-17 13:37:08 -0400

    Add column handler tests

--- tests/api_v2_handlers_tests.py
+++ tests/api_v2_handlers_tests.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 from nose.tools import ok_
 
 from moncli.api_v2 import handlers, constants
-from moncli.enums import BoardKind
+from moncli.enums import BoardKind, ColumnType
 
 EXECUTE_QUERY_PATCH = 'moncli.api_v2.requests.execute_query'
 
@@ -59,33 +59,45 @@ def test_archive_board(execute_query):
 def test_create_column(execute_query):
 
     # Arrange
+    title = 'Text Column One'
+    execute_query.return_value = {constants.CREATE_COLUMN: {'id': 'text_column_1', 'title': title, 'type': 'text'}}
 
     # Act
+    new_column = handlers.create_column('', '1', title, ColumnType.text, 'id', 'title', 'type')
     
     # Assert
-    pass
+    ok_(new_column != None)
+    ok_(new_column['title'] == title)
+    ok_(new_column['type'] == ColumnType.text.name)
 
 
 @patch(EXECUTE_QUERY_PATCH)
 def test_change_column_value(execute_query):
 
     # Arrange
+    execute_query.return_value = {constants.CHANGE_COLUMN_VALUE: {'id': '1'}}
 
     # Act
+    updated_item = handlers.change_column_value('', '1', 'text_column_1', '1', 'Hello, world!', 'id')
     
     # Assert
-    pass
+    ok_(updated_item != None)
+    ok_(updated_item['id'] == '1')
 
 
 @patch(EXECUTE_QUERY_PATCH)
 def test_change_multiple_column_value(execute_query):
 
     # Arrange
+    column_values = {'text_column_1': 'Let\'s eat, Grandma!', 'numbers_column_1': 8675309}
+    execute_query.return_value = {constants.CHANGE_MULTIPLE_COLUMN_VALUES: {'id': '1'}}
 
     # Act
+    updated_item = handlers.change_multiple_column_value('', '1', '1', column_values, 'id')
     
     # Assert
-    pass
+    ok_(updated_item != None)
+    ok_(updated_item['id'] == '1')
 
 
 @patch(EXECUTE_QUERY_PATCH)
