25e613ee8fb871ed45b063eefa8a7ee4b280f5ca
AShatz <andrew.shatz@trix.solutions>
2019-11-02 23:52:40 -0700

    Group archive test

--- tests/entities_group_tests.py
+++ tests/entities_group_tests.py
@@ -28,3 +28,27 @@ def test_should_duplicate_a_group(duplicate_group, create_group, create_board, g
     # Assert
     ok_(group != None)
     eq_(group.title, 'Group 1 (copy)')
+
+
+@patch.object(e.client.MondayClient, 'get_me')
+@patch('moncli.api_v2.create_board')
+@patch('moncli.api_v2.create_group')
+@patch('moncli.api_v2.archive_group')
+def test_should_archive_a_group(archive_group, create_group, create_board, get_me):
+
+    # Arrange
+    get_me.return_value = GET_ME_RETURN_VALUE
+    create_board.return_value = {'id': '1', 'name': 'Test Board 1'}
+    create_group.return_value = {'id': 'group_01', 'title': 'Group 1'}
+    archive_group.return_value = {'id': 'group_01', 'title': 'Group 1', 'archived': 'true'}
+    client = e.client.MondayClient(USERNAME, '', '')
+    board = client.create_board('Test Board 1', BoardKind.public)
+    group = board.add_group('Group 1')
+
+    # Act 
+    group = group.archive()
+
+    # Assert
+    ok_(group != None)
+    eq_(group.title, 'Group 1')
+    eq_(group.archived, 'true')
