64bf5d59536f229a34c22fcf9ff6de6a1a2718e0
AShatz <andrew.shatz@trix.solutions>
2019-11-02 23:54:11 -0700

    Group delete test

--- tests/entities_group_tests.py
+++ tests/entities_group_tests.py
@@ -52,3 +52,27 @@ def test_should_archive_a_group(archive_group, create_group, create_board, get_m
     ok_(group != None)
     eq_(group.title, 'Group 1')
     eq_(group.archived, 'true')
+
+
+@patch.object(e.client.MondayClient, 'get_me')
+@patch('moncli.api_v2.create_board')
+@patch('moncli.api_v2.create_group')
+@patch('moncli.api_v2.delete_group')
+def test_should_delete_a_group(delete_group, create_group, create_board, get_me):
+
+    # Arrange
+    get_me.return_value = GET_ME_RETURN_VALUE
+    create_board.return_value = {'id': '1', 'name': 'Test Board 1'}
+    create_group.return_value = {'id': 'group_01', 'title': 'Group 1'}
+    delete_group.return_value = {'id': 'group_01', 'title': 'Group 1', 'deleted': 'true'}
+    client = e.client.MondayClient(USERNAME, '', '')
+    board = client.create_board('Test Board 1', BoardKind.public)
+    group = board.add_group('Group 1')
+
+    # Act 
+    group = group.delete()
+
+    # Assert
+    ok_(group != None)
+    eq_(group.title, 'Group 1')
+    eq_(group.deleted, 'true')
