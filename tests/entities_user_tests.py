9adc64d86d40288369a3c9b11b56fdc3d486eb87
AShatz <andrew.shatz@trix.solutions>
2019-11-03 01:55:08 -0700

    User send notification test

--- tests/entities_user_tests.py
+++ tests/entities_user_tests.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 from nose.tools import ok_, eq_, raises
 
 import moncli.entities as e
+import moncli.enums as en
 
 USERNAME = 'test.user@foobar.org' 
 GET_ME_RETURN_VALUE = e.user.User(**{'creds': None, 'id': '1', 'email': USERNAME})
@@ -9,7 +10,7 @@ GET_ME_RETURN_VALUE = e.user.User(**{'creds': None, 'id': '1', 'email': USERNAME
 
 @patch.object(e.client.MondayClient, 'get_me')
 @patch('moncli.api_v2.get_users')
-def test_should_get_the_account(get_users, get_me):
+def test_user_should_get_the_account(get_users, get_me):
 
     # Arrange
     get_me.return_value = GET_ME_RETURN_VALUE
@@ -30,7 +31,7 @@ def test_should_get_the_account(get_users, get_me):
 @patch.object(e.client.MondayClient, 'get_me')
 @patch('moncli.api_v2.get_users')
 @patch('moncli.api_v2.get_teams')
-def test_should_get_teams(get_teams, get_users, get_me):
+def test_user_should_get_teams(get_teams, get_users, get_me):
 
     # Arrange
     get_me.return_value = GET_ME_RETURN_VALUE
@@ -46,3 +47,23 @@ def test_should_get_teams(get_teams, get_users, get_me):
     ok_(teams != None)
     eq_(len(teams), 1)
     eq_(teams[0].name, 'Team 1')
+
+
+@patch.object(e.client.MondayClient, 'get_me')
+@patch('moncli.api_v2.get_users')
+@patch('moncli.api_v2.create_notification')
+def test_user_should_send_a_notification(create_notification, get_users, get_me):
+
+    # Arrange 
+    get_me.return_value = GET_ME_RETURN_VALUE
+    get_users.return_value = [{'id': '1', 'email': 'test.user@foobar.org', 'teams': [{'id': '1'}]}]
+    create_notification.return_value = {'id': '1', 'text': 'Text 1'}
+    client = e.client.MondayClient(USERNAME, '', '')
+    user = client.get_users()[0]
+
+    # Act 
+    notification = user.send_notification('Text 1', '2', en.NotificationTargetType.Post)
+
+    # Assert
+    ok_(notification != None)
+    ok_(notification.text, 'Text 1')
