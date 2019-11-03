336f31ee6c87a2b33d3aaeecb794e7c970c07764
AShatz <andrew.shatz@trix.solutions>
2019-11-03 00:15:37 -0700

    Group get items test

--- tests/entities_group_tests.py
+++ tests/entities_group_tests.py
@@ -99,3 +99,32 @@ def test_should_create_an_item(create_item, create_group, create_board, get_me):
     # Assert
     ok_(item != None)
     eq_(item.name, 'Item 1')
+
+
+@patch.object(e.client.MondayClient, 'get_me')
+@patch('moncli.api_v2.create_board')
+@patch('moncli.api_v2.create_group')
+@patch('moncli.api_v2.get_boards')
+@patch('moncli.api_v2.get_items')
+def test_should_retrieve_a_list_of_items(get_items, get_boards, create_group, create_board, get_me):
+
+    # Arrange
+    get_me.return_value = GET_ME_RETURN_VALUE
+    create_board.return_value = {'id': '1', 'name': 'Test Board 1'}
+    create_group.return_value = {'id': 'group_01', 'title': 'Group 1'}
+    get_boards.return_value = [
+        {'id': '1', 'groups': [
+            {'id': 'group_01', 'items':[
+                {'id': '1', 'name': 'Item 1', 'board': {'id': '1'}}]}]}]
+    get_items.return_value = [{'id': '1', 'name': 'Item 1', 'board': {'id': '1'}}]
+    client = e.client.MondayClient(USERNAME, '', '')
+    board = client.create_board('Test Board 1', BoardKind.public)
+    group = board.add_group('Group 1')
+
+    # Act 
+    items = group.get_items()
+
+    # Assert
+    ok_(items != None)
+    eq_(len(items), 1)
+    eq_(items[0].name, 'Item 1')
