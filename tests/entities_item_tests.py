2b568f45a4876bcd56977394afcde23451979673
AShatz <andrew.shatz@trix.solutions>
2019-11-03 17:10:05 -0700

    Item delete test

--- tests/entities_item_tests.py
+++ tests/entities_item_tests.py
@@ -246,3 +246,23 @@ def test_item_should_archive_item(archive_item, get_items, get_me):
     # Assert 
     ok_(item != None)
     eq_(item.name, 'Test Item 01')
+
+
+@patch.object(e.client.MondayClient, 'get_me')
+@patch('moncli.api_v2.get_items')
+@patch('moncli.api_v2.delete_item')
+def test_item_should_delete_item(delete_item, get_items, get_me):
+
+    # Arrange
+    get_me.return_value = GET_ME_RETURN_VALUE
+    get_items.return_value = [{'id': '1', 'name': 'Test Item 01', 'board': {'id': '1'}}]
+    delete_item.return_value = {'id': '1', 'name': 'Test Item 01', 'board': {'id': '1'}}
+    client = e.client.MondayClient(USERNAME, '', '')
+    item = client.get_items()[0]
+
+    # Act
+    item = item.delete()
+
+    # Assert 
+    ok_(item != None)
+    eq_(item.name, 'Test Item 01')
