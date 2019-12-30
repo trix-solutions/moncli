cd3ca493e3c67cb8cdaecb6e4f024270f0bfbe05
AShatz <andrew.shatz@trix.solutions>
2019-12-29 22:11:23 -0500

    Import dependency issue resolved.

--- moncli/entities/__init__.py
+++ moncli/entities/__init__.py
@@ -1,6 +1,6 @@
 from .user import User, Team, Account
 from .item import Item
 from .group import Group
-from .board import Board, InvalidColumnValue
 from .column_value import ColumnValue, create_column_value
+from .board import Board, InvalidColumnValue
 from .objects import MondayClientCredentials, Column, Update, Notification, Tag, Plan 
