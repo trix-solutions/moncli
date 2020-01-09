3bb6c3d6d43916b3db2a184523539e666d5f8e66
AShatz <andrew.shatz@trix.solutions>
2020-01-08 19:08:39 -0500

    Add nested argument configs for items

--- moncli/api_v2/constants.py
+++ moncli/api_v2/constants.py
@@ -122,15 +122,27 @@ ITEMS_OPTIONAL_PARAMS = {
     'page': ArgumentValueKind.Int,
     'ids': ArgumentValueKind.List,
     'newest_first': ArgumentValueKind.Bool,
-    'updates_limit': ArgumentValueKind.Int,
-    'updates_page': ArgumentValueKind.Int
+    'column_values': {
+        'ids': ArgumentValueKind.List
+    },
+    'updates': {
+        'limit': ArgumentValueKind.Int,
+        'page': ArgumentValueKind.Int
+    }
 }
 
 ITEMS_BY_COLUMN_VALUES_OPTIONAL_PARAMS = {
     'limit': ArgumentValueKind.Int,
     'page': ArgumentValueKind.Int,
     'column_type': ArgumentValueKind.String,
-    'state': ArgumentValueKind.Enum
+    'state': ArgumentValueKind.Enum,
+    'column_values': {
+        'ids': ArgumentValueKind.List
+    },
+    'updates': {
+        'limit': ArgumentValueKind.Int,
+        'page': ArgumentValueKind.Int
+    }
 }
 
 CREATE_ITEM_OPTIONAL_PARAMS ={
