0dc941bc82510edafd8ff459fc0958e322dee15d
AShatz <andrew.shatz@trix.solutions>
2020-01-09 00:53:15 -0500

    Added arguments decorator method

--- moncli/decorators.py
+++ moncli/decorators.py
@@ -3,10 +3,6 @@ import copy
 def default_field_list(field_list: list):
     def wrap(func):
         def inner(instance, *args, **kwargs):
-            # This loop prevents duplicate args and kwargs from messing up the decorator.
-            for key in [key for key in kwargs.keys()]:
-                if key in args or key in field_list:
-                    args = (*args, kwargs.pop(key))
             for arg in args:
                 # Input arg list contains a default field
                 if arg in field_list:
@@ -18,4 +14,15 @@ def default_field_list(field_list: list):
             args = (*args, *field_list)
             return func(instance, *args, **kwargs)
         return inner         
+    return wrap
+
+def optional_arguments(arguments: dict):
+    def wrap(func):
+        def inner(instance, *args, **kwargs):
+            # This loop prevents decorator from breaking when required params are used as kwargs.
+            for key in [key for key in kwargs.keys()]:
+                if key not in arguments.keys():
+                    args = (*args, kwargs.pop(key))
+            return func(instance, *args, **kwargs)
+        return inner
     return wrap
