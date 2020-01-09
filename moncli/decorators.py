a14f708bd1fdaf70a244d94c0d7be7bddbf10b65
AShatz <andrew.shatz@trix.solutions>
2020-01-08 23:41:34 -0500

    Finalize decorator

--- moncli/decorators.py
+++ moncli/decorators.py
@@ -1,12 +1,21 @@
+import copy
+
 def default_field_list(field_list: list):
     def wrap(func):
         def inner(instance, *args, **kwargs):
+            # This loop prevents duplicate args and kwargs from messing up the decorator.
+            for key in [key for key in kwargs.keys()]:
+                if key in args or key in field_list:
+                    args = (*args, kwargs.pop(key))
             for arg in args:
-                # Execute function as is with input return fields if 
-                # arg list contains default or nested field
-                if arg in field_list or len(arg.split('.')) > 1:
+                # Input arg list contains a default field
+                if arg in field_list:
+                    return func(instance, *args, **kwargs)
+                # Input arg list contains a nested field
+                if type(arg) is str and len(arg.split('.')) > 1:
                     return func(instance, *args, **kwargs)
             # Add field list if no args are present
-            return func(instance, *args, *field_list, **kwargs)
+            args = (*args, *field_list)
+            return func(instance, *args, **kwargs)
         return inner         
     return wrap
