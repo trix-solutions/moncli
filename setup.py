0ba84aba74797894446bf1750c612839bb6a13db
Andrew <andrew.shatz@trix.solutions>
2020-06-10 09:24:52 -0400

    Misc. - Move nose Dependency to tests_require

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ config = {
     'url': r'https://github.com/trix-solutions/moncli',
     'download_url': r'https://github.com/trix-solutions/moncli',
     'author_email': 'andrew.shatz@trix.solutions',
-    'version': '0.1.5',
+    'version': '0.1.6',
     'install_requires': [
-        'nose>=1.3.7',
         'requests>=2.22.0'
     ],
+    'tests_require': [
+        'nose>=1.3.7'
+    ]
     'packages': [
         'moncli',
         'moncli.api_v1',
