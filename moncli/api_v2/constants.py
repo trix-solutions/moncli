c5c4b6c52c50b7950babc100d96af177ff06d182
AShatz <andrew.shatz@trix.solutions>
2020-10-15 14:44:04 -0400

    Update constants for list params

--- moncli/api_v2/constants.py
+++ moncli/api_v2/constants.py
@@ -65,20 +65,20 @@ ADD_FILE_TO_COLUMN = 'add_file_to_column'
 BOARDS_OPTIONAL_PARAMS = {
     'limit': ArgumentValueKind.Int,
     'page': ArgumentValueKind.Int,
-    'ids': ArgumentValueKind.List,
+    'ids': (ArgumentValueKind.List, ArgumentValueKind.Int),
     'board_kind': ArgumentValueKind.Enum,
     'state': ArgumentValueKind.Enum,
     'newest_first': ArgumentValueKind.Bool,
     'groups': {
-        'ids': ArgumentValueKind.List,
+        'ids': (ArgumentValueKind.List, ArgumentValueKind.Int),
         'items': {
-            'ids': ArgumentValueKind.List,
+            'ids': (ArgumentValueKind.List, ArgumentValueKind.Int),
             'limit': ArgumentValueKind.Int,
             'page': ArgumentValueKind.Int
         }
     },
     'items': {
-        'ids': ArgumentValueKind.List,
+        'ids': (ArgumentValueKind.List, ArgumentValueKind.Int),
         'limit': ArgumentValueKind.Int,
         'page': ArgumentValueKind.Int
     },
@@ -128,10 +128,10 @@ DELETE_GROUP_OPTIONAL_PARAMS = {
 ITEMS_OPTIONAL_PARAMS = {
     'limit': ArgumentValueKind.Int,
     'page': ArgumentValueKind.Int,
-    'ids': ArgumentValueKind.List,
+    'ids': (ArgumentValueKind.List, ArgumentValueKind.Int),
     'newest_first': ArgumentValueKind.Bool,
     'column_values': {
-        'ids': ArgumentValueKind.List
+        'ids': (ArgumentValueKind.List, ArgumentValueKind.Int),
     },
     'updates': {
         'limit': ArgumentValueKind.Int,
@@ -145,7 +145,7 @@ ITEMS_BY_COLUMN_VALUES_OPTIONAL_PARAMS = {
     'column_type': ArgumentValueKind.String,
     'state': ArgumentValueKind.Enum,
     'column_values': {
-        'ids': ArgumentValueKind.List
+        'ids': (ArgumentValueKind.List, ArgumentValueKind.Int),
     },
     'updates': {
         'limit': ArgumentValueKind.Int,
@@ -184,7 +184,7 @@ CREATE_NOTIFICATION_OPTIONAL_PARAMS ={
 }
 
 TAGS_OPTIONAL_PARAMS = {
-    'ids': ArgumentValueKind.List
+    'ids': (ArgumentValueKind.List, ArgumentValueKind.Int)
 }
 
 CREATE_OR_GET_TAG_OPTIONAL_PARAMS = {
@@ -192,14 +192,14 @@ CREATE_OR_GET_TAG_OPTIONAL_PARAMS = {
 }
 
 USERS_OPTIONAL_PARAMS = {
-    'ids': ArgumentValueKind.List,
+    'ids': (ArgumentValueKind.List, ArgumentValueKind.Int),
     'kind': ArgumentValueKind.Enum,
     'newest_first': ArgumentValueKind.String,
     'limit': ArgumentValueKind.Int
 }
 
 TEAMS_OPTIONAL_PARAMS = {
-    'ids': ArgumentValueKind.List
+    'ids': (ArgumentValueKind.List, ArgumentValueKind.Int)
 }
 
 CREATE_WEBHOOK_OPTIONAL_PARAMS = {
