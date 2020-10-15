ba6b72aa9382dbe08e0ef40a0443e81e99abd0df
AShatz <andrew.shatz@trix.solutions>
2020-10-15 12:55:15 -0400

    Add API v2 file endpoint

--- moncli/constants.py
+++ moncli/constants.py
@@ -4,6 +4,7 @@ DATE_FORMAT = '%Y-%m-%d'
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 API_V2_ENDPOINT = 'https://api.monday.com/v2'
+API_V2_FILE_ENDPOINT = 'https://api.monday.com/v2/file'
 
 # Column Types
 COLUMN_NAME = 'name'
