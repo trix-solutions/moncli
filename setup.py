c5b5588c1a95076d64f3e50fd2fd35d04cd268cb
AShatz <andrew.shatz@trix.solutions>
2020-10-15 13:06:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ config = {
     'url': r'https://github.com/trix-solutions/moncli',
     'download_url': r'https://github.com/trix-solutions/moncli',
     'author_email': 'andrew.shatz@trix.solutions',
-    'version': '0.1.3',
+    'version': '1.0.0',
     'install_requires': [
         'nose>=1.3.7',
-        'requests>=2.22.0',
-        'pytz>=2019.3',
-        'pycountry>=19.8.18',
-        'deprecated>=1.2.7'
+        'requests>=2.24.0',
+        'pytz>=2020.1',
+        'pycountry>=20.7.3',
+        'deprecated>=1.2.10',
+        'schematics>=2.1.0'
     ],
     'packages': [
         'moncli',
