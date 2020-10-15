ec4c01738ad7e9e28be48a599d05f06d2b5577e1
AShatz <andrew.shatz@trix.solutions>
2020-10-15 17:00:02 -0400

    Add upload file to update test

--- tests/entities_update_tests.py
+++ tests/entities_update_tests.py
@@ -55,3 +55,27 @@ def test_update_should_return_creator_of_update_reply(get_users, get_updates, ge
     # Assert
     ok_(reply != None)
     eq_(reply.creator.to_primitive(), GET_ME_RETURN_VALUE.to_primitive())
+
+
+@patch.object(MondayClient, 'get_me')
+@patch('moncli.api_v2.get_updates')
+@patch('moncli.api_v2.add_file_to_update')
+def test_should_get_updates(add_file_to_update, get_updates, get_me):
+
+    # Arrange 
+    id = '1'
+    body = 'Hello, world!'
+    get_me.return_value = GET_ME_RETURN_VALUE
+    get_updates.return_value = [{'id': id, 'body': body}]
+    add_file_to_update.return_value = {'id': '12345', 'name': '33.jpg', 'url': 'https://test.monday.com/12345/33.jpg'}
+    client = MondayClient(USERNAME, '', '')
+    update = client.get_updates()[0]
+
+    # Act
+    asset = update.add_file('/Users/test/33.jpg')
+
+    # Assert
+    ok_(asset != None)
+    eq_(asset.id, '12345')
+    eq_(asset.name, '33.jpg')
+    eq_(asset.url, 'https://test.monday.com/12345/33.jpg')
