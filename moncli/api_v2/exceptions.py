ce1c7775bf5d72413d1c5486f1453a508b79eb35
AShatz <andrew.shatz@trix.solutions>
2020-10-17 08:13:34 -0400

    Update requests and exceptions

--- moncli/api_v2/exceptions.py
+++ moncli/api_v2/exceptions.py
@@ -1,7 +1,8 @@
 class MondayApiError(Exception):
 
-    def __init__(self, query: str, status_code: int, messages: list):
+    def __init__(self, query: str, status_code: int, error_code: str, messages: list):
 
         self.query = query
         self.status_code = status_code
+        self.error_code = error_code
         self.messages = messages
