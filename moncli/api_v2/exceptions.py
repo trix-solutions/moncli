470734862af08fa3cd9dd565dbb5d4392a2c81fb
AShatz <andrew.shatz@trix.solutions>
2020-10-22 11:37:27 -0400

    Monday API exceptions

--- moncli/api_v2/exceptions.py
+++ moncli/api_v2/exceptions.py
@@ -1,4 +1,18 @@
 class MondayApiError(Exception):
+    """monday.com API exception
+
+    __________
+    Properties
+    __________
+    query : `str`
+        The submitted query resulting in error.
+    status_code : `int`
+        The http status code of the response.
+    error_code : `str`
+        The monday.com API error code.
+    messages : `list[str]`
+        The list of error messages.
+    """
 
     def __init__(self, query: str, status_code: int, error_code: str, messages: list):
 
