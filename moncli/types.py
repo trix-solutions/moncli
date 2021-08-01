1d5cbc4709224be3b13aa33ee15290e31323d98e
AShatz <andrew.shatz@trix.solutions>
2021-08-01 16:38:45 -0400

    Add checkbox type

--- moncli/types.py
+++ moncli/types.py
@@ -2,16 +2,14 @@ import json
 
 from schematics.types import BaseType
 
+from moncli.entities import column_value as cv
+
 class MondayType(BaseType):
 
-    def __init__(self, column_value_type: type, id: str = None, title: str = None, *args, **kwargs):
+    def __init__(self, id: str = None, title: str = None, *args, **kwargs):
         self.original_value = None
-        
-        metadata = {
-            'module' : column_value_type.__module__,
-            'name' : column_value_type.__name__
-        }
-        
+        metadata = {}
+
         if not id and not title:
             raise MondayTypeError('"id" or "title" parameter is required.')
         if id:
@@ -23,11 +21,30 @@ class MondayType(BaseType):
 
     def to_native(self, value, context=None):
         self.original_value = value.value
-
         self.metadata['id'] = value.id
         self.metadata['title'] = value.title
-
         return json.loads(value.value)
+
+
+class CheckboxType(MondayType):
+
+    def to_native(self, value, context = None):
+        value = super().to_native(value, context=context)
+        try:
+            return bool(value['checked'])
+        except:
+            return False
+
+    def to_primitive(self, value, context = None):
+        return {'checked': value}
+
+
+class TextType(MondayType):
+
+    def to_primitive(self, value, context=None):
+        if not value:
+            return ''
+        return value
 
 
 class MondayTypeError(Exception):
