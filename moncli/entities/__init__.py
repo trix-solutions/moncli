85c7dd3839155533a6a99d42836d8a1b85acfbf8
AShatz <andrew.shatz@trix.solutions>
2021-08-12 10:37:32 -0400

    Add method to board entity.

--- moncli/entities/__init__.py
+++ moncli/entities/__init__.py
@@ -1,3 +1,4 @@
+from .objects import MondayClientCredentials, ActivityLog, BoardView, Column, ColumnCollection, Notification, Tag, Plan, Webhook, Workspace, StatusSettings, DropdownSettings
 from .user import User, Team, Account
 from .asset import Asset
 from .item import Item, UpdateNotFound
@@ -5,5 +6,4 @@ from .group import Group
 from .column_value import ColumnValue, ColumnValueCollection, create_column_value
 from .board import Board, InvalidColumnValue
 from .update import Update, Reply
-from .objects import MondayClientCredentials, ActivityLog, BoardView, Column, ColumnCollection, Notification, Tag, Plan, Webhook, Workspace, StatusSettings, DropdownSettings
 from .client import MondayClient
