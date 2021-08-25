e9d1d8197c25eb276901689a6bd0b84b6f3a01ed
AShatz <andrew.shatz@trix.solutions>
2021-08-25 12:03:11 -0400

    CP - Rename constants and move config.

--- moncli/api_v2/config.py
+++ moncli/api_v2/config.py
@@ -1,25 +1,28 @@
 from .graphql import ArgumentValueKind
 
-## Operation methods
 
+## API Configurations
+API_V2_ENDPOINT = 'https://api.monday.com/v2'
+API_V2_FILE_ENDPOINT = 'https://api.monday.com/v2/file'
+CONNECTION_TIMEOUT = 10
+
+
+## Operation methods
 # Boards
 BOARDS = 'boards'
 CREATE_BOARD = 'create_board'
 ARCHIVE_BOARD = 'archive_board'
 ADD_SUBSCRIBERS_TO_BOARD = 'add_subscribers_to_board'
 DELETE_SUBSCRIBERS_FROM_BOARD = 'delete_subscribers_from_board'
-
 # Columns
 CREATE_COLUMN = 'create_column'
 CHANGE_COLUMN_VALUE = 'change_column_value'
 CHANGE_MULTIPLE_COLUMN_VALUES = 'change_multiple_column_values'
-
 # Groups
 DUPLICATE_GROUP = 'duplicate_group'
 CREATE_GROUP = 'create_group'
 ARCHIVE_GROUP = 'archive_group'
 DELETE_GROUP = 'delete_group'
-
 # Items
 ITEMS = 'items'
 ITEMS_BY_COLUMN_VALUES = 'items_by_column_values'
@@ -31,50 +34,35 @@ MOVE_ITEM_TO_GROUP = 'move_item_to_group'
 ARCHIVE_ITEM = 'archive_item'
 DELETE_ITEM = 'delete_item'
 DUPLICATE_ITEM = 'duplicate_item'
-
 # Updates
 UPDATES = 'updates'
 CREATE_UPDATE = 'create_update'
 DELETE_UPDATE = 'delete_update'
-
 # Notifications
 CREATE_NOTIFICATION = 'create_notification'
-
 # Tags
 TAGS = 'tags'
 CREATE_OR_GET_TAG = 'create_or_get_tag'
-
 # Users 
 USERS = 'users'
-
 # Teams
 TEAMS = 'teams'
-
 # Me
 ME = 'me'
-
 # Account
 ACCOUNT = 'account'
-
 # Webhooks
 CREATE_WEBHOOK = 'create_webhook'
 DELETE_WEBHOOK = 'delete_webhook'
-
 # Workspace
 CREATE_WORKSPACE = 'create_workspace'
-
 # Files
 ASSETS = 'assets'
 ADD_FILE_TO_UPDATE = 'add_file_to_update'
 ADD_FILE_TO_COLUMN = 'add_file_to_column'
 
 
-##
-
-
 ## Default Fields
-
-
 DEFAULT_BOARD_QUERY_FIELDS = [
     'id', 
     'name', 
--- moncli/config.py
+++ moncli/config.py
@@ -2,8 +2,3 @@
 DATE_FORMAT = '%Y-%m-%d'
 TIME_FORMAT = '%H:%M:%S'
 ZULU_FORMAT = '{}T{}.%fZ'
-
-# API Configurations
-API_V2_ENDPOINT = 'https://api.monday.com/v2'
-API_V2_FILE_ENDPOINT = 'https://api.monday.com/v2/file'
-CONNECTION_TIMEOUT = 10
