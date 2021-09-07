af5cc636b3f841925557e280bc2e49d8e642d258
Andrew <andrew.shatz@trix.solutions>
2021-09-07 10:41:33 -0400

    Misc. Format Documentation (#122)

--- moncli/entities/client.py
+++ moncli/entities/client.py
@@ -1464,56 +1464,20 @@ class MondayClient():
         
         Return Fields
 
-                account : `moncli.entities.User`
-                    The user's account.
-                birthday : `str`
-                    The user's birthday.
-                country_code : `str`
-                    The user's country code.
-                created_at : `str`
-                    The user's creation date.
-                email : `str`
-                    The user's email.
-                enabled : `bool`
-                    Is the user enabled or not.
-                id : `str`
-                    The user's unique identifier.
-                is_guest : `bool`
-                    Is the user a guest or not.
-                is_pending : `bool`
-                    Is the user a pending user.
-                is_view_only : `bool`
-                    Is the user a view only user or not.
-                join_date : `str`
-                    The date the user joined the account.
-                location : `str`
-                    The user' location.
-                mobile_phone : `str`
-                    The user's mobile phone number.
-                name : `str`
-                    The user's name.
-                phone : `str`
-                    The user's phone number.
-                photo_original : `str`
-                    The user's photo in the original size.
-                photo_small : `str`
-                    The user's photo in small size (150x150).
-                photo_thumb : `str`
-                    The user's photo in thumbnail size (100x100).
-                photo_thumb_small : `str`
-                    The user's photo in small thumbnail size (50x50).
-                photo_tiny : `str`
-                    The user's photo in tiny size (30x30).
-                teams : `list[moncli.entities.Team]`
-                    The teams the user is a member in.
-                time_zone_identifier : `str`
-                    The user's time zone identifier.
-                title : `str`
-                    The user's title.
-                url : `str`
-                    The user's profile url.
-                utc_hours_diff : `int`
-                    The user's UTC hours difference.
+            id : `int`
+                The team's unique identifier.
+            name : `str`
+                The team's name.
+            picture_url : `str`
+                The team's picture url.
+            users : `moncli.entities.user.User`
+                The users in the team.
+            name : `str`
+                The team's name.
+            picture_url : `str`
+                The team's picture url.
+            users : `moncli.entities.user.User`
+                The users in the team.
 
         Optional Arguments
 
@@ -1547,8 +1511,6 @@ class MondayClient():
                     List of teams deleted from the workspace
             
             Return Fields
-            
-                Return Fields
 
                 id : `int`
                     The team's unique identifier.
