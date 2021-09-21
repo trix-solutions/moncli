af51df1bba443be1c72ceba0859768fe42a67a64
Aswath Muralidharan <aswathm78@gmail.com>
2021-09-21 18:57:38 +0530

    Initial Commit

--- moncli/entities/column_value/simple.py
+++ moncli/entities/column_value/simple.py
@@ -1,10 +1,45 @@
 from .base import SimpleNullValue, ComplexNullValue
 from .constants import SIMPLE_NULL_VALUE
 from ...error import ColumnValueError
+from datetime import datetime, timedelta
+from moncli.config import DATE_FORMAT,TIME_FORMAT
+import pytz
 class DateValue(ComplexNullValue):
     """A date column value."""
-    pass
+    native_type = datetime.datetime 
+    allow_casts = (int, str)
+    has_time = False
+
+    def _convert(self,value):
+        try:
+            new_time = datetime.strptime(value['time'], TIME_FORMAT)
+            new_date = datetime.strptime(value['date'], DATE_FORMAT)
+            self.has_time = True
+            new_date = pytz.timezone('UTC').localize(new_date)
+            new_date = new_date + timedelta(hours=new_time.hours, minutes=new_time.minutes, seconds=new_time.seconds)
+            date_value = new_date.astimezone(datetime.now().astimezone().tzinfo) 
+            return date_value
+
+        except KeyError:
+            new_date = datetime.strptime(value['date'], DATE_FORMAT)
+            return new_date
         
+    def _cast(self,value):
+        if isinstance(value,int):
+            try: 
+                new_date_value =  datetime.fromtimestamp(value)
+                return new_date_value
+
+            except ValueError:
+                raise ColumnValueError(
+                    'invalid_unix_date',
+                     self.id,
+                    'Unable to convert "{}" from UNIX timestamp.'.format(value)
+                )
+        if isinstance(value, str):
+            try:
+                date=value.split()[0]
+                time=value.split()[1]
 
 class DropdownValue(ComplexNullValue):
     """A dropdown column value."""
