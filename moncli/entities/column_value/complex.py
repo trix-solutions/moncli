3568c0070a6fa7c9f6b21c2ed0844a92bb47101e
Aswath Muralidharan <aswathm78@gmail.com>
2021-09-23 21:19:00 +0530

    Initial Commit

--- moncli/entities/column_value/complex.py
+++ moncli/entities/column_value/complex.py
@@ -1,4 +1,13 @@
+from .objects import Week
 from .base import ColumnValue
+from datetime import datetime
+from moncli import enums
+from datetime import datetime, timedelta
+from .base import SimpleNullValue, ComplexNullValue
+from .constants import SIMPLE_NULL_VALUE
+from ...error import ColumnValueError
+from moncli.config import DATE_FORMAT,TIME_FORMAT
+import pytz
 
 class CheckboxValue(ColumnValue):
     """A checkbox column value."""
@@ -40,6 +49,30 @@ class TimezoneValue(ColumnValue):
     pass
 
 
-class WeekValue(ColumnValue):
+class WeekValue(ComplexNullValue):
     """A week column value."""
-    pass
+    
+    native_type = Week
+    allow_casts = (dict)
+    
+    
+    def _convert(self, value):
+        try:
+            start = datetime.strptime(value['startDate'], DATE_FORMAT)
+            end = datetime.strptime(value['endDate'], DATE_FORMAT)
+            return Week(start=start, end=end)
+        except KeyError:
+            return {}
+        
+    def _cast(self, value):
+        try:
+            start_date = value['startDate']
+            end_date = value['endDate']
+        except (ValueError,KeyError):
+            raise ColumnValueError('invalid_week_data', self.id, 'Unable to convert "{}" to Week value of end date.'.format(value))
+
+        return self._convert(value)
+
+        
+    def _format(self):
+            return {'startDate': self.value.start, 'endDate': self.value.end}
