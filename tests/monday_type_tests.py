816e13238b1d9305fad603825140b1cacf77faea
Aswath Muralidharan <aswathm78@gmail.com>
2021-09-30 21:29:01 +0530

    Update requested changes

--- tests/monday_type_tests.py
+++ tests/monday_type_tests.py
@@ -84,3 +84,64 @@ def test_should_succeed_when_to_primitive_returns_str_when_passed_in_an_int_floa
     eq_(int_value,'1')
     eq_(float_value,'1.0')
     eq_(str_value,'text')
+
+def test_should_succeed_when_to_native_returns_an_int_or_float_when_passed_a_numbervalue_value_with_api_data():
+    # Arrange
+    number_type = t.NumberType(id='number_1')
+    column_value = cv.create_column_value(ColumnType.numbers, id='number_1', value=json.dumps(1))
+
+    # Act
+    int_value = number_type.to_native(column_value)
+
+    # Assert 
+    eq_(int_value, 1)
+    
+def test_should_succeed_when_to_native_returns_a_int_or_float_when_passed_a_int_or_float_value():
+    # Arrange
+    number_type = t.NumberType(id=1)
+
+    # Act
+
+    int_value = number_type.to_native(1)
+    float_value  = number_type.to_native(1.0)
+
+    # Assert
+    eq_(int_value,1)
+    eq_(float_value,1.0)
+
+def test_should_succeed_when_to_native_returns_a_none_when_passed_a_none():
+    # Arrange
+    number_type = t.NumberType(id=1)
+
+    # Act
+
+    value = number_type.to_native(None)
+
+    # Assert
+    eq_(value,None)
+
+
+def test_should_succeed_when_to_primitive_returns_empty_str_when_passed_a_none():
+    # Arrange
+    number_type = t.NumberType(id=1)
+
+    # Act
+    value = number_type.to_primitive(None)
+
+    # Assert
+    eq_(value,'')
+
+
+def test_should_succeed_when_to_primitive_returns_export_string_when_passed_a_int_or_float_value():
+    # Arrange
+    number_type = t.NumberType(id=1)
+
+    # Act
+    int_value = number_type.to_primitive(1)
+    float_value = number_type.to_primitive(1.0)
+
+    
+
+    # Assert
+    eq_(int_value,'1')
+    eq_(float_value,'1.0')
