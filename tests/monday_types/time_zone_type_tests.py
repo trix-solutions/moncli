0e44794dd60f4e611758c8e7a0dc66c9f3e2c9ab
AShatz <andrew.shatz@trix.solutions>
2021-10-01 14:55:24 -0400

    Formatting fixes.

--- tests/monday_types/time_zone_type_tests.py
+++ tests/monday_types/time_zone_type_tests.py
@@ -1,15 +1,9 @@
-import json
-import pytz
-from pytz.exceptions import UnknownTimeZoneError
-from schematics.exceptions import ValidationError,DataError
+from schematics.exceptions import ValidationError
 from nose.tools import eq_,raises
-from unittest.mock import patch
 
 from moncli import entities as en
-from moncli import types as t
-from moncli import client
 from moncli.enums import ColumnType
-from moncli.models import MondayModel
+from moncli.types import TimeZoneType
 
 
 def test_should_succeed_when_to_native_returns_a_str_when_passed_a_timezonevalue_value_with_api_data_to_world_clock_type():
@@ -22,7 +16,7 @@ def test_should_succeed_when_to_native_returns_a_str_when_passed_a_timezonevalue
     column_value.value = 'America/New_York'
 
     # Act
-    timezone_type = t.TimeZoneType(title=title)
+    timezone_type = TimeZoneType(title=title)
     value = timezone_type.to_native(column_value)
 
     # Assert
@@ -43,7 +37,7 @@ def test_should_succeed_when_to_native_returns_a_none_when_passed_a_none_to_worl
 def test_should_succeed_when_to_primitive_returns_an_empty_dict_when_passed_a_none_to_world_clock_type():
 
     # Arrange
-    timezone_type = t.TimeZoneType(title='Time Zone Column 1')
+    timezone_type = TimeZoneType(title='Time Zone Column 1')
 
     # Act
     timezone_value = timezone_type.to_primitive(None)
@@ -61,7 +55,7 @@ def test_should_succeed_when_to_primitive_returns_export_dict_when_passed_in_a_s
     column_value.value = 'America/New_York'
 
     # Act
-    timezone_type = t.TimeZoneType(title=title)
+    timezone_type = TimeZoneType(title=title)
     value = timezone_type.to_primitive(column_value)
 
     # Assert
@@ -71,7 +65,7 @@ def test_should_succeed_when_to_primitive_returns_export_dict_when_passed_in_a_s
 def test_timezone_type_should_raise_validation_error_when_validate_timezone_receives_invalid_timezone_str():
 
     # Arrange
-    timezone_type = t.TimeZoneType(id='timezone_type_1')
+    timezone_type = TimeZoneType(id='timezone_type_1')
 
     # Act
     timezone_type.validate_timezone(value='Invalid/Timezone')
